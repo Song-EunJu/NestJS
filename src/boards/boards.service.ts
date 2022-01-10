7c17b451c9f4e2acff40b11ede3423518f8b83a0
Song-EunJu <jd06280@naver.com>
2022-01-10 19:15:20 +0900

    [feat]: #3 - 아이디로 특정 게시물 조회

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './boards.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -10,15 +11,16 @@ export class BoardsService {
         return this.boards;
     }
 
-    createBoard(title: string, description: string){
+    createBoard(createBoardDto: CreateBoardDto){
         const board: Board = {
             id: uuid(),
-            title,
-            description,
+            ...createBoardDto,
             status: BoardStatus.PUBLIC
         }
         this.boards.push(board);
         return board;
     }
-
+    getBoardById(id: string): Board{
+        return this.boards.find((board) => board.id === id);
+    }
 }
