90cb3ab1cd5c3867bf650a64b352507f8b697183
Song-EunJu <jd06280@naver.com>
2022-01-11 15:04:02 +0900

    [add] typeorm config 파일

---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export const typeORMConfig : TypeOrmModuleOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'board-app',
+    entities: [__dirname + '../**/*.entity.{js,ts}'],
+    // 엔티티를 이용해서 DB 테이블 생성해주므로 엔티티 파일이 어디에 있는지 설정해줌
+    synchronize: true
+    // 애플리케이션을 다시 실행할 때 엔티티 안에서 수정된 컬럼의 길이, 타입, 변경값들을
+    // 해당 테이블을 drop한 후 다시 생성해준다.
+}
